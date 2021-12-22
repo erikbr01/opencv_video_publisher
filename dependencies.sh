1f9fae620c77e0455d60a51b85c6c0d46b5d1fbf
Erik Bauer <erik.hamburg@gmx.de>
2021-12-22 15:13:48 +0100

    added dependencies install script

---
+++ dependencies.sh
@@ -0,0 +1,11 @@
+sudo apt install cmake build-essential pkg-config git
+
+sudo apt install libjpeg-dev libtiff-dev libjasper-dev libpng-dev libwebp-dev libopenexr-dev
+
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libdc1394-22-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
+
+sudo apt install libgtk-3-dev libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+
+sudo apt install libatlas-base-dev liblapacke-dev gfortran
+
+sudo apt install libhdf5-dev libhdf5-103
